352ab04f92d3e3da36c472cd0daacffa359d108c
Hieu Nguyen <142032868+hieunv2011@users.noreply.github.com>
2023-08-17 12:18:17 +0700

    Add files via upload

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,13 @@
+import React from "react";
+import{
+    Text, View
+}from "react-native";
+
+const OrderDelivery =()=>{
+    return(
+        <View>
+            <Text>OrderDelivery</Text>
+        </View>
+    )
+}
+export default OrderDelivery;
---
+++ screens/Restaurant.js
@@ -0,0 +1,13 @@
+import React from "react";
+import{
+    Text, View
+}from "react-native";
+
+const Restaurant =()=>{
+    return(
+        <View>
+            <Text>Restaurant</Text>
+        </View>
+    )
+}
+export default Restaurant;
